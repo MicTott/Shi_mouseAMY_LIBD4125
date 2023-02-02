e849268d33266e3a4a176493f346b736ad55b4c9
Michael Totty <mictott@gmail.com>
2023-02-02 12:55:02 -0500

    the last attempt worked for single file. Updated s01_convert_SRA_to_FASTQ.sg to loop through 2 files.

--- code/s01_convert_SRA_to_FASTQ.sh
+++ code/s01_convert_SRA_to_FASTQ.sh
@@ -21,20 +21,16 @@ echo "Task id: ${SGE_TASK_ID}"
 # load SRAtoolkit
 module load sratoolkit
 
-# change directory to ensure local. sra files are found
-# otherwise, fastq-dump will re-download .sra files and take way longer
-
-fasterq-dump --split-files /dcs04/lieber/marmaypag/Shi_mouseAMY_LIBD4125/raw-data/SRA/SRR15666960 -O /dcs04/lieber/marmaypag/Shi_mouseAMY_LIBD4125/raw-data/FASTQ/
-
 # -gzip created zipped file
 # --spilt-files to seperate forward and reverse reads into seperate files
 # -O output directory
 # all files SRR15666956 SRR15666957 SRR15666958 SRR15666959 SRR15666960 SRR15666961
-#for f in SRR15666960 SRR15666961
-#do
-#    echo "Processing $f"
-#    fasterq-dump --split-files SRA/$f -O FASTQ/
-#done
+for f in SRR15666960 SRR15666961
+do
+    echo "Processing $f"
+    fasterq-dump --split-files /dcs04/lieber/marmaypag/Shi_mouseAMY_LIBD4125/raw-data/SRA/$f -O /dcs04/lieber/marmaypag/Shi_mouseAMY_LIBD4125/raw-data/FASTQ/
+
+done
 
 echo "**** Job ends ****"
 date
